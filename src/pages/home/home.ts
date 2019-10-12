43043cfb10b9ba0b338f74d07d68d930bdf17f29
junho7 <junho7@gmail.com>
2019-10-12 16:45:17 -0700

    first commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  public tapCounter: number = 0;
+  constructor(public navCtrl: NavController) {
+
+  }
+  buttonTapped(){
+    this.tapCounter++;
+  }
+  reset(){
+    this.tapCounter = 0;
+  }
+
+}
